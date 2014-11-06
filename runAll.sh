4d49c716fd21a29a21e7bba512e17c826114d8e1
HoldenCaulfieldICL <adalyac@gmail.com>
2014-11-06 04:26:29 +0000

    modifying val sets to remove unknown about why accuracy measure varies lots

---
+++ runAll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+time nohup ./build/tools/caffe train -solver task/clamp/solver.prototxt -weights oxford/small.weights
+time nohup ./build/tools/caffe train -solver task/inadcl/solver.prototxt -weights oxford/small.weights
+time nohup ./build/tools/caffe train -solver task/misal/solver.prototxt -weights oxford/small.weights
+time nohup ./build/tools/caffe train -solver task/scrape/solver.prototxt -weights oxford/small.weights
+time nohup ./build/tools/caffe train -solver task/soil_high/solver.prototxt -weights oxford/small.weights
+time nohup ./build/tools/caffe train -solver task/water_high/solver.prototxt -weights oxford/small.weights
+time nohup ./build/tools/caffe train -solver task/unsuit/solver.prototxt -weights oxford/small.weights
