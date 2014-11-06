7ee83f5d3da5e125c3c8289087d5bc271917de5b
HoldenCaulfieldICL <adalyac@gmail.com>
2014-11-06 05:40:17 +0000

    training

--- runAll.sh
+++ runAll.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-time nohup ./build/tools/caffe train -solver task/inadcl/solver.prototxt -weights oxford/small.weights
-time nohup ./build/tools/caffe train -solver task/misal/solver.prototxt -weights oxford/small.weights
-time nohup ./build/tools/caffe train -solver task/scrape/solver.prototxt -weights oxford/small.weights
-time nohup ./build/tools/caffe train -solver task/soil_high/solver.prototxt -weights oxford/small.weights
-time nohup ./build/tools/caffe train -solver task/water_high/solver.prototxt -weights oxford/small.weights
-time nohup ./build/tools/caffe train -solver task/unsuit/solver.prototxt -weights oxford/small.weights
-time nohup ./build/tools/caffe train -solver task/clamp/solver.prototxt -weights oxford/small.weights
+time nohup ./build/tools/caffe train -solver task/inadcl/solver.prototxt -weights oxford/small.weights 2>&1 | tee task/inadcl/logs/train.log
+time nohup ./build/tools/caffe train -solver task/misal/solver.prototxt -weights oxford/small.weights 2>&1 | tee task/misal/logs/train.log
+time nohup ./build/tools/caffe train -solver task/scrape/solver.prototxt -weights oxford/small.weights 2>&1 | tee task/scrape/logs/train.log
+time nohup ./build/tools/caffe train -solver task/soil_high/solver.prototxt -weights oxford/small.weights 2>&1 | tee task/soil_high/logs/train.log
+time nohup ./build/tools/caffe train -solver task/water_high/solver.prototxt -weights oxford/small.weights 2>&1 | tee task/water_high/logs/train.log
+time nohup ./build/tools/caffe train -solver task/unsuit/solver.prototxt -weights oxford/small.weights 2>&1 | tee task/unsuit/logs/train.log
+time nohup ./build/tools/caffe train -solver task/clamp/solver.prototxt -weights oxford/small.weights 2>&1 | tee task/clamp/logs/train.log
