2e802eb826736bf8d6c2b21747d7641c29629b9a
HoldenCaulfieldICL <adalyac@gmail.com>
2014-11-09 03:05:41 +0000

    train update

--- task/inadcl_o/resume_training.sh
+++ task/inadcl_o/resume_training.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+cd ../..
 ./build/tools/caffe train \
     --solver=task/inadcl_o/solver.prototxt \
-    --snapshot=task/inadcl_o/none/_iter_3000.caffemodel
+
+    # --snapshot=task/inadcl_o/none/_iter_3000.caffemodel
