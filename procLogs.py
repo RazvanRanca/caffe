f14834473dd8c27cf5ff13ef48e247db6ed7054a
HoldenCaulfieldRye <adalyac@gmail.com>
2014-11-10 08:39:00 +0000

    added multJoints script

--- procLogs.py
+++ procLogs.py
@@ -1,16 +1,20 @@
 import cPickle
 import sys
 
-flags = "clamp_1flag inadcl misal scrape soil_high unsuit water_high".split()
+flags = "clamp_1flag inadcl misal soil_high scrape unsuit water_high".split()
 posThresh = {"clamp_1flag" : 0.5, "inadcl" : 0.5, "misal" : 0.5, "scrape" : 0.5, "soil_high" : 0.5, "unsuit" : 0.5, "water_high" : 0.5}
 
-def imgsToJoint(preds):
-  pass
+def imgsToJoint(flag, preds):
+  avg = sum(preds) / float(len(preds)) 
+  if avg > posThresh[flag]:
+    return 1
+  return 0
 
 if __name__ == "__main__":
+  if len(sys.argv) != 4:
+    raise Exception("Need multJoints,log folder and output file as parameters")
   jointMap = {}
-  if len(sys.argv) > 1:
-    npDir = sys.argv[2]
+  npDir = sys.argv[2]
 
   with open(sys.argv[1], 'r') as f:
     for line in f:
@@ -19,20 +23,29 @@ if __name__ == "__main__":
         jointMap[int(img)] = int(line[0])
  
   jointPos = {}
+  joints = set() 
   for flag in flags:
     pos = {}
-    try:
-      with open(npDir + "/" + flag + ".npy", 'r') as f:
-        rezs = cPickle.load(f)
-        for fn, rez in rezs:
-          fNo = fn.split('.')[0].split('/')[-1]
-          jNo = jointMap(int(fNo))
-          try:
-            pos[jNo].append(rez[1])
-          except:
-            pos[jNo] = [rez[1]]
-    except:
-      pass
-    flagPos[flag] = pos
+    with open(npDir + "/" + flag + ".npy", 'r') as f:
+      rezs = cPickle.load(f)
+      for fn, rez in rezs:
+	fNo = fn.split('.')[0].split('/')[-1]
+	jNo = jointMap[int(fNo)]
+        joints.add(jNo)
+	try:
+	  pos[jNo].append(rez[1])
+	except:
+	  pos[jNo] = [rez[1]]
+    jointPos[flag] = pos
 
-  print flagPos
+  with open(sys.argv[3], 'w') as f:
+    out = "Joint, " + ', '.join(flags)
+    print out
+    f.write(out + "\n")
+    for joint in sorted(list(joints)):
+      out = str(joint)
+      for flag in flags:
+	out += ", " + str(imgsToJoint(flag, jointPos[flag][joint]))
+      print out
+      f.write(out + "\n")
+ 
