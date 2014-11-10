1bbc779161cc4eef3f981a54501c1b2ec11ac4f1
HoldenCaulfieldRye <adalyac@gmail.com>
2014-11-10 07:22:34 +0000

    update

--- procLogs.py
+++ procLogs.py
@@ -1,4 +1,5 @@
 import cPickle
+import sys
 
 flags = "clamp_1flag inadcl misal scrape soil_high unsuit water_high".split()
 posThresh = {"clamp_1flag" : 0.5, "inadcl" : 0.5, "misal" : 0.5, "scrape" : 0.5, "soil_high" : 0.5, "unsuit" : 0.5, "water_high" : 0.5}
@@ -8,7 +9,10 @@ def imgsToJoint(preds):
 
 if __name__ == "__main__":
   jointMap = {}
-  with open("multJoints", 'r') as f:
+  if len(sys.argv) > 1:
+    npDir = sys.argv[2]
+
+  with open(sys.argv[1], 'r') as f:
     for line in f:
       line = line.strip().split()
       for img in line[1:]:
@@ -18,7 +22,7 @@ if __name__ == "__main__":
   for flag in flags:
     pos = {}
     try:
-      with open(flag + ".npy", 'r') as f:
+      with open(npDir + "/" + flag + ".npy", 'r') as f:
         rezs = cPickle.load(f)
         for fn, rez in rezs:
           fNo = fn.split('.')[0].split('/')[-1]
