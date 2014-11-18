981ff1f260df42c40fc2ca43ed5d32e54f4841f5
HoldenCaulfieldRye <adalyac@gmail.com>
2014-11-18 00:55:13 +0000

    per class accuracy fixed

--- src/caffe/layers/per_class_accuracy_layer.cpp
+++ src/caffe/layers/per_class_accuracy_layer.cpp
@@ -32,7 +32,7 @@ void PerClassAccuracyLayer<Dtype>::Reshape(
   CHECK_EQ(bottom[1]->width(), 1);
   top[0]->Reshape(1, 1, 1, 1);
   int dim = bottom[0]->count() / bottom[0]->num();
-  int accuracies_count = dim + 2;
+  int accuracies_count = dim * 2;
   top[0]->Reshape(1, 1, 1, accuracies_count);
   accuracies_.Reshape(1, 1, 1, accuracies_count);    
   labels_count_.Reshape(1, 1, 1, dim);  
@@ -47,16 +47,14 @@ void PerClassAccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& botto
   // Dtype accuracy = 0;
   int num = bottom[0]->num();
   int dim = bottom[0]->count() / bottom[0]->num();
-  int accuracies_count = dim + 2;
+  int accuracies_count = dim * 2;
 
-  Dtype* labels_count = labels_count_.mutable_cpu_data();
-  Dtype* accuracies = accuracies_.mutable_cpu_data();
-  caffe_set(labels_count_.count(), Dtype(FLT_MIN), labels_count);
-  caffe_set(accuracies_.count(), Dtype(FLT_MIN), accuracies);
+  Dtype* accuracyLabelPairs = accuracies_.mutable_cpu_data(); // accuracy i is in pos 2*i and label count i in position 2*i+1
+  caffe_set(accuracies_.count(), Dtype(FLT_MIN), accuracyLabelPairs);
   
   for (int i = 0; i < num; ++i) {
     //count freq of each class
-    labels_count[static_cast<int>(bottom_label[i])] += 1.0;
+    accuracyLabelPairs[static_cast<int>(bottom_label[i])*2 + 1] += 1.0;
     //determine whether correctly classified
     Dtype maxval = -FLT_MAX;
     int max_id = 0;
@@ -68,19 +66,19 @@ void PerClassAccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& botto
       }
     }
     if (max_id == static_cast<int>(bottom_label[i]))
-      accuracies[static_cast<int>(bottom_label[i])] += 1.0;
+      accuracyLabelPairs[static_cast<int>(bottom_label[i])*2] += 1.0;
   }
 
-  for (int j = 0; j < dim; ++j) {
+  //for (int j = 0; j < dim; ++j) {
     //accuracy averaged across cases
-    accuracies[accuracies_count-1] += accuracies[j];
+    //accuracies[accuracies_count-1] += accuracies[j];
     //accuracy for class j
-    accuracies[j] /= static_cast<float>(labels_count[j]);
+    //accuracies[j] /= static_cast<float>(labels_count[j]);
     //accuracy averaged across classes
-    accuracies[accuracies_count-2] += accuracies[j];
-  }
-  accuracies[accuracies_count-1] /= static_cast<float>(num);
-  accuracies[accuracies_count-2] /= static_cast<float>(dim);
+    //accuracies[accuracies_count-2] += accuracies[j];
+  //}
+  //accuracies[accuracies_count-1] /= static_cast<float>(num);
+  //accuracies[accuracies_count-2] /= static_cast<float>(dim);
 
   // std::cout << "Accuracies: ";
   // for (int j = 0; j < accuracies_count; ++j) {
@@ -88,9 +86,9 @@ void PerClassAccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& botto
   // }
   // std::cout << std::endl;
   
-  // LOG(INFO) << "Accuracies, class by class: " << accuracy;
+  //LOG(INFO) << "Accuracies, class by class: " << accuracyLabelPairs[0] << " out of " << accuracyLabelPairs[1];
   //can I do this or does 
-  caffe_copy(accuracies_count, accuracies, top[0]->mutable_cpu_data());
+  caffe_copy(accuracies_count, accuracyLabelPairs, top[0]->mutable_cpu_data());
   // (*top)[0]->mutable_cpu_data()[0] = accuracy / num;
   // (*top)[0]->mutable_cpu_data()[0] = 1;
   // (*top)[0]->mutable_cpu_data()[0] = 2;
